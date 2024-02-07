bd424a7b346f30dea074b8d957c0ea5f0256bb67
Ankita Sikdar <115947852+AnkitaSikdar005@users.noreply.github.com>
2024-02-07 19:01:15 +0530

    Update Q1_temp.py

--- Python_ITWorkshop_3rdSem/week1/Q1_temp.py
+++ Python_ITWorkshop_3rdSem/week1/Q1_temp.py
@@ -1,5 +1,12 @@
+#method1
 def temp_con(temp):
     return (9*temp/5)+32
 
 cel= float(input("ENTER A TEMPERATURE IN CELCIUS: "))
 print("TEMPERATURE IN FARENHEIT IS: ",temp_con(cel))
+
+
+#method2
+cel1= float(input("ENTER A TEMPERATURE IN CELCIUS: "))
+f=int((9*cel1/5 + 32))
+print("TEMPERATURE IN FARENHEIT IS: ",f)
